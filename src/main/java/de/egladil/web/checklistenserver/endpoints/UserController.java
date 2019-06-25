5ca5e06503e0ba7eeffc6f85fd938927164a1379
Heike Winkelvoß <public@egladil.de>
2019-06-25 23:22:35 +0200

    fehlerhaftes insert korrigiert

--- src/main/java/de/egladil/web/checklistenserver/endpoints/UserController.java
+++ src/main/java/de/egladil/web/checklistenserver/endpoints/UserController.java
@@ -8,9 +8,9 @@ package de.egladil.web.checklistenserver.endpoints;
 import java.security.Principal;
 
 import javax.enterprise.context.RequestScoped;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
